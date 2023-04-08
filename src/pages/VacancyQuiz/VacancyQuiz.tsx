04538a64e1d03517c53c4274636a27348c44788d
Yourec <rulet.puzyaka@yandex.ru>
2023-04-08 20:56:51 +0300

    AdminPanel for Quizes and Users

--- src/pages/VacancyQuiz/VacancyQuiz.tsx
+++ src/pages/VacancyQuiz/VacancyQuiz.tsx
@@ -1,39 +1,141 @@
-import React from 'react';
-import { Card, Col, Row } from "antd";
+import React, { useState } from 'react';
+import { Button, Card, Col, Row, AutoComplete, Input, Space, SelectProps, Typography } from "antd";
 import { Widget } from "../../components/Widget/Widget";
 import { Simulate } from "react-dom/test-utils";
 import styles from './VacancyQuiz.module.css'
-import click = Simulate.click;
-import { quizData, vacancyList } from '../../app/dataExample';
+import { quizData } from '../../app/dataExample';
 import { Link } from 'react-router-dom';
 import OpenQuizQuestion from '../../components/OpenQuizQuestion/OpenQuizQuestion';
 import { MultipleQuizQuestion } from '../../components/MultipleQuizQuestion/MultipleQuizQuestion';
 import QuizQuestion from '../../components/QuizQuestion/QuizQuestion';
+const { Title } = Typography;
 
 
 interface VacancyQuizI {
     /* vacancyList: VacancyT[], */
 }
-export const VacancyQuiz: React.FC<VacancyQuizI> = () => {
-    return (
-        <div className={styles.select_vacancy_wrapper}>
-            <Col className={styles.quizList}>
-                {
-                   quizData.length && quizData.map((quiz, index) => (
-                        quiz.type === 'open' 
-                        ?
-                        <OpenQuizQuestion correctAnswer={quiz.correctAnswer as string} question={quiz.question} onSubmit={()=>{}} />
-                        :
-                        quiz.type === 'multiple' 
-                        ? 
-                        <MultipleQuizQuestion correctAnswers={quiz.correctAnswer as string[] } options={quiz.options} question={quiz.question} onSubmit={()=>{}} />
-                        :
-                       quiz.type === 'standart' && <QuizQuestion correctAnswer={quiz.correctAnswer as string} options={quiz.options} question={quiz.question} onSubmit={()=>{}}/>
+const getRandomInt = (max: number, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-                    ))
-                }
-            </Col>
-        </div>
+const searchResult = (query: string) =>
+    new Array(getRandomInt(5))
+        .join('.')
+        .split('.')
+        .map((_, idx) => {
+            const category = `${query}${idx}`;
+            return {
+                value: category,
+                label: (
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <span>
+                            Найдено {query} on{' '}
+                            <a
+                                href={`https://s.taobao.com/search?q=${query}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {category}
+                            </a>
+                        </span>
+                        <span>{getRandomInt(200, 100)} results</span>
+                    </div>
+                ),
+            };
+        });
+
+export const VacancyQuiz: React.FC<VacancyQuizI> = () => {
+    const [activeTabKey1, setActiveTabKey1] = useState<string>('quizes');
+    const [options, setOptions] = useState<SelectProps<object>['options']>([]);
+
+    const handleSearch = (value: string) => {
+        setOptions(value ? searchResult(value) : []);
+    };
+
+    const onSelect = (value: string) => {
+        console.log('onSelect', value);
+    };
+
+    const onTab1Change = (key: string) => {
+        setActiveTabKey1(key);
+    };
+
+    const tabList = [
+        {
+            key: 'users',
+            tab: 'Пользователи',
+        },
+        {
+            key: 'quizes',
+            tab: 'Тесты'
+        },
+    ];
+
+    const extra = (
+
+        <Space wrap>
+            {
+                activeTabKey1 === 'users'
+                    ?
+                    <></>
+                    :
+                    <>
+                        <Button type='primary' ghost>По направлению</Button>
+                        <Button type='primary' ghost>По баллам</Button>
+                    </>
+            }
+
+
+            <AutoComplete
+                dropdownMatchSelectWidth={252}
+                style={{ width: 300 }}
+                options={options}
+                onSelect={onSelect}
+                onSearch={handleSearch}
+            >
+                <Input.Search size="middle" placeholder="Поиск" enterButton />
+            </AutoComplete>
+        </Space>
+    )
+
+
+    const quizes = <Col className={styles.quizList}>
+        {
+            quizData.length && quizData.map((quiz, index) => (
+                quiz.type === 'open'
+                    ?
+                    <OpenQuizQuestion correctAnswer={quiz.correctAnswer as string} question={quiz.question} onSubmit={() => { }} />
+                    :
+                    quiz.type === 'multiple'
+                        ?
+                        <MultipleQuizQuestion correctAnswers={quiz.correctAnswer as string[]} options={quiz.options} question={quiz.question} onSubmit={() => { }} />
+                        :
+                        quiz.type === 'standart' && <QuizQuestion correctAnswer={quiz.correctAnswer as string} options={quiz.options} question={quiz.question} onSubmit={() => { }} />
+
+            ))
+        }
+    </Col>
+
+    const contentList: Record<string, React.ReactNode> = {
+        users: <p>Список пользователей</p>,
+        quizes: quizes,
+    };
+
+    return (
+        <Card className={styles.select_vacancy_wrapper}
+            style={{ width: '100%' }}
+            title={<Title level={4} style={{ margin: 0 }}>{`Название теста`}</Title>}
+            tabList={tabList}
+            activeTabKey={activeTabKey1}
+            onTabChange={onTab1Change}
+            extra={extra}
+
+        >
+            {contentList[activeTabKey1]}
+        </Card>
     );
 };
 
