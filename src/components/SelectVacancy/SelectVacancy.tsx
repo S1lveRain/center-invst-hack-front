c1a7679355612b899bd9e411996ea161f8becda4
Yourec <rulet.puzyaka@yandex.ru>
2023-04-08 15:09:04 +0300

    SelectVacancy render

---
+++ src/components/SelectVacancy/SelectVacancy.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Row } from "antd";
+import { Widget } from "../Widget/Widget";
+import { Simulate } from "react-dom/test-utils";
+import styles from './SelectVacancy.module.css'
+import click = Simulate.click;
+
+export type VacancyT = {
+    title: string;
+    description: string
+}
+
+interface SelectVacancyI {
+    vacancyList: VacancyT[],
+}
+export const SelectVacancy: React.FC<SelectVacancyI> = ({vacancyList}) => {
+    return (
+        <div className={styles.select_vacancy_wrapper}>
+            <div className={styles.select_vacancy_title}>
+                <h1>В нашей компании требуются разработчики по направлениям:</h1>
+            </div>
+            <Row className={styles.vacancyList}>
+                {
+                    vacancyList.length && vacancyList.map((vacancy, index) => (
+                        <a className={styles.vacancy_container} key={vacancy.title + index}>
+                            <Widget
+                             title={vacancy.description}
+                             value={vacancy.title}
+                            />
+                        </a>
+                    ))
+                }
+            </Row>
+        </div>
+    );
+};
+
+export default SelectVacancyI;
