d2743637ca6d8e009cd5d231397eba57e250666d
Rain <silverain1qw@gmail.com>
2023-04-08 15:00:54 +0300

    BgContainer error fix

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ const App: React.FC = () => {
       </Header>
       <Content style={{ padding: "0 50px" }}>
         <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
-        <Layout style={{ padding: "24px 0", background: colorBgContainer }}>
-          <Sider style={{ background: colorBgContainer }} width={200}>
+        <Layout style={{ padding: "24px 0", background: token.colorBgContainer }}>
+          <Sider style={{ background: token.colorBgContainer }} width={200}>
               <Menu
                   mode="inline"
                   defaultSelectedKeys={["1"]}
