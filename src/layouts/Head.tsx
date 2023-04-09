3ac6a2ed67ca1e07be2c1d2be5c1849f7b7c84a7
DULCETq <maks.lebedev0404@mail.ru>
2023-04-09 10:16:40 +0300

    fix

--- src/layouts/Head.tsx
+++ src/layouts/Head.tsx
@@ -7,69 +7,69 @@ import { useSelector } from "react-redux";
 import { getUser } from "../app/slices/authSlice";
 import { useGetUserQuery } from "../app/services/UserApi";
 import { UserOutlined } from "@ant-design/icons";
-export const Head = () => {
-  const userId = useSelector(getUser());
-  const { data: currentUser, error, isLoading } = useGetUserQuery(userId);
-  const { token } = theme.useToken();
-  return (
-    <Header
-      className="header"
-      style={{
-        backgroundColor: "white",
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
-      <Link to={"/"} style={{ display: "flex", alignItems: "center" }}>
-        <img
-          src={logo}
-          style={{
-            width: 35,
-            height: 35,
-            backgroundPosition: "center",
-            margin: 10,
-          }}
-          alt={"broken"}
-        />
-        <h1 style={{ color: "#13AA09", margin: 0 }}>Work</h1>
-      </Link>
 
-      {/* <Link to="/personalpage"> */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          color: "#13AA09",
-          margin: 0,
-        }}
-      >
-        {isLoading ? (
-          <>Loading...</>
-        ) : (
-          <div>
-            <h4 style={{ margin: "-15px 10px 0 0" }}>{currentUser.email}</h4>
-            <p
-              style={{
-                margin: "-10px 0 0 0",
-                position: "absolute",
-                right: 105,
-                top: 20,
-              }}
+export const Head = () => {
+    const userId = useSelector(getUser());
+    const { data: currentUser, error, isLoading } = useGetUserQuery(userId);
+    const { token } = theme.useToken();
+
+    return (
+        <Header
+            className="header"
+            style={{
+                backgroundColor: "white",
+                display: "flex",
+                justifyContent: "space-between",
+            }}
+        >
+            <Link to={"/"} style={{ display: "flex", alignItems: "center" }}>
+                <img
+                    src={logo}
+                    style={{
+                        width: 35,
+                        height: 35,
+                        backgroundPosition: "center",
+                        margin: 10,
+                    }}
+                    alt={"broken"}
+                />
+                <h1 style={{ color: "#13AA09", margin: 0 }}>Work</h1>
+            </Link>
+
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    color: "#13AA09",
+                    margin: 0,
+                }}
             >
-              {currentUser.role}
-            </p>
-          </div>
-        )}
-        {currentUser && (
-          <Avatar
-            size="default"
-            icon={<UserOutlined />}
-            style={{ margin: "0 10px 0 0" }}
-          />
-        )}
-      </div>
-      {/* </Link> */}
-    </Header>
-  );
+                {isLoading ? (
+                    <>Loading...</>
+                ) : (
+                    <div>
+                        {currentUser && <h4 style={{ margin: "-15px 10px 0 0" }}>{currentUser.email}</h4>}
+                        {currentUser && <p
+                            style={{
+                                margin: "-10px 0 0 0",
+                                position: "absolute",
+                                right: 105,
+                                top: 20,
+                            }}
+                        >
+                            {currentUser.role}
+                        </p>}
+                    </div>
+                )}
+                {currentUser && (
+                    <Avatar
+                        size="default"
+                        icon={<UserOutlined />}
+                        style={{ margin: "0 10px 0 0" }}
+                    />
+                )}
+            </div>
+        </Header>
+    );
 };
